6f1915f3ac33df82191be2d6c2c284f164716adc
Sergey <levashov04sergey@gmail.com>
2021-05-09 19:52:21 +0300

    12. Framer Motion

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import React from 'react';
 import AboutUs from "./pages/aboutUs";
 //Global Style
 import GlobalStyle from "./components/globalStyle";
+import ContactUs from "./pages/contactUs";
+import OurWork from "./pages/ourWork";
+import Nav from "./components/nav";
+import MovieDetail from "./pages/movieDetail";
+//Router
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             <GlobalStyle/>
-            <AboutUs/>
+            <Nav/>
+            <Switch>
+                <Route path="/" exact>
+                    <AboutUs/>
+                </Route>
+                <Route path="/work" exact>
+                    <OurWork/>
+                </Route>
+                <Route path="/work/:id">
+                    <MovieDetail/>
+                </Route>
+                <Route  path="/contact">
+                    <ContactUs/>
+                </Route>
+            </Switch>
         </div>
     );
 }
