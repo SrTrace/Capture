1764d1ae5f0fe5b73ca70b32add5db06685b743c
Sergey <levashov04sergey@gmail.com>
2021-05-09 09:18:38 +0300

    5. Intro to Style Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 //Import Pages
 import AboutUs from "./pages/aboutUs";
+//Global Style
+import GlobalStyle from "./components/globalStyle";
 
 function App() {
     return (
         <div className="App">
-          <AboutUs/>
+            <GlobalStyle/>
+            <AboutUs/>
         </div>
     );
 }
