3daf0051bc4789598262b15746ff32a8d9c372a8
Sergey <levashov04sergey@gmail.com>
2021-05-10 10:27:36 +0300

    13. Page Transition

--- src/App.js
+++ src/App.js
@@ -8,27 +8,33 @@ import OurWork from "./pages/ourWork";
 import Nav from "./components/nav";
 import MovieDetail from "./pages/movieDetail";
 //Router
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, useLocation} from 'react-router-dom';
+//Animation
+import {AnimatePresence} from "framer-motion";
 
 function App() {
+    const location = useLocation();
+    console.log(location);
     return (
         <div className="App">
             <GlobalStyle/>
             <Nav/>
-            <Switch>
-                <Route path="/" exact>
-                    <AboutUs/>
-                </Route>
-                <Route path="/work" exact>
-                    <OurWork/>
-                </Route>
-                <Route path="/work/:id">
-                    <MovieDetail/>
-                </Route>
-                <Route  path="/contact">
-                    <ContactUs/>
-                </Route>
-            </Switch>
+            <AnimatePresence exitBeforeEnter>
+                <Switch location={location} key={location.pathname}>
+                    <Route path="/" exact>
+                        <AboutUs/>
+                    </Route>
+                    <Route path="/work" exact>
+                        <OurWork/>
+                    </Route>
+                    <Route path="/work/:id">
+                        <MovieDetail/>
+                    </Route>
+                    <Route path="/contact">
+                        <ContactUs/>
+                    </Route>
+                </Switch>
+            </AnimatePresence>
         </div>
     );
 }
--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -3,15 +3,23 @@ import React from 'react';
 import AboutSection from "../components/aboutSection";
 import ServicesSection from "../components/servicesSection";
 import FaqSection from "../components/faqSection";
+//Animation
+import {motion} from "framer-motion";
+import {pageAnimation} from "../animation";
 
 const AboutUs = () => {
     return (
-    <>
-        <AboutSection/>
-        <ServicesSection/>
-        <FaqSection/>
-    </>
-        );
-}
+        <motion.div
+            variants={pageAnimation}
+            initial="hidden"
+            animate="show"
+            exit="exit"
+        >
+            <AboutSection/>
+            <ServicesSection/>
+            <FaqSection/>
+        </motion.div>
+    );
+};
 
 export default AboutUs;
