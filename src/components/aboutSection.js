d503f4bda8c07d7017dfecd122c2a9e61b3f15b8
Sergey <levashov04sergey@gmail.com>
2021-05-10 12:14:30 +0300

    Animation

--- src/components/aboutSection.js
+++ src/components/aboutSection.js
@@ -3,6 +3,8 @@ import home1 from '../img/home1.png';
 import {About, Description, Image, Hide} from "../style";
 //Framer Motion
 import {motion} from 'framer-motion';
+import {titleAnim, fade,  photoAnim} from "../animation";
+import Wave from "./wave";
 
 const AboutSection = () => {
     return (
@@ -10,24 +12,29 @@ const AboutSection = () => {
             <Description>
                 <motion.div>
                     <Hide>
-                        <motion.h2>We work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             your <span>dreams</span> come
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>true.</motion.h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing
-                    skills.</p>
-                <button>Contact us</button>
+                <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing
+                    skills.</motion.p>
+                <motion.button variants={fade}>Contact us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with camera"/>
+                <motion.img
+                    variants={photoAnim}
+                    src={home1}
+                    alt="guy with camera"
+                />
             </Image>
+            <Wave/>
         </About>
     );
 };
